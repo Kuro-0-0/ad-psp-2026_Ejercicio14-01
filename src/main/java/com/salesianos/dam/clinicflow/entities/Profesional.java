023453e70e011ea5f8fc2eda9dcecbd6703709d4
Pablo Garcia - Clase <garcia.mapab24@triana.salesianos.edu>
2026-01-14 09:35:43 +0100

    Base entity Profesional ready

--- src/main/java/com/salesianos/dam/clinicflow/entities/Profesional.java
+++ src/main/java/com/salesianos/dam/clinicflow/entities/Profesional.java
@@ -1,4 +1,26 @@
 package com.salesianos.dam.clinicflow.entities;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import lombok.*;
+
+@Entity
+@AllArgsConstructor @NoArgsConstructor
+@ToString @Getter @Setter
+@Builder
 public class Profesional {
+
+    /*
+    * id
+    * nombre
+    * especialidad
+    * */
+
+    @Id @GeneratedValue
+    private Long id;
+
+    private String nombre;
+    private String especialidad;
+
 }
