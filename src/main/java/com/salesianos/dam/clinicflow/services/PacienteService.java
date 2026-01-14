dd99c81aa83d374ecaa0b2eb3a92ddc7bb922d81
Pablo Garcia - Clase <garcia.mapab24@triana.salesianos.edu>
2026-01-14 10:06:39 +0100

    Service CRUD, Entity Not Found Exception & getAll ready

---
+++ src/main/java/com/salesianos/dam/clinicflow/services/PacienteService.java
@@ -0,0 +1,42 @@
+package com.salesianos.dam.clinicflow.services;
+
+import com.salesianos.dam.clinicflow.entities.Paciente;
+import com.salesianos.dam.clinicflow.exceptions.notFound.PacienteNotFoundException;
+import com.salesianos.dam.clinicflow.repositories.PacienteRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+public class PacienteService {
+
+    private final PacienteRepository repository;
+
+    /* -- CRUD -- */
+
+    public Paciente create(Paciente p) {
+        return repository.save(p);
+    }
+
+    public Paciente read(Long id) {
+        return repository.findById(id).orElseThrow(() -> new PacienteNotFoundException(id));
+    }
+
+    public Paciente update(Long id, Paciente p) {
+        Paciente pOriginal = repository.findById(id).orElseThrow(() -> new PacienteNotFoundException(id));;
+        return repository.save(pOriginal.modify(p));
+    }
+
+    public void delete(Long id) {
+        Paciente p = repository.findById(id).orElseThrow(() -> new PacienteNotFoundException(id));
+        repository.delete(p);
+    }
+
+    /* -- EXTRAS -- */
+    public List<Paciente> getAll() {
+        return repository.findAll();
+    }
+
+}
